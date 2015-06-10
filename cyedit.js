93714e0ddfa23369ae1e5de44b3343a0c8367abd
Clément Renaud <clement.renaud@gmail.com>
2015-06-10 08:30:47 +0200

    refactor methods and db sync

--- cyedit.js
+++ cyedit.js
@@ -1,64 +1,107 @@
-Elements = new Mongo.Collection("elements");
+var Nodes = new Mongo.Collection("nodes");
+var Edges = new Meteor.Collection("edges");
 
 function initCy(){
+
   cy = cytoscape({
     container: document.getElementById('cy')
   });
-  
+
   Tracker.autorun(function(){
     cy.elements().remove();
-    cy.add( Elements.find().fetch() ); // will get errors for duplicates but it will still work...
+
+    cy.add( Nodes.find().fetch() );
+    cy.add( Edges.find().fetch() );
+
+    cy.reset()
   });
-  
-  // more cytoscape code can go here...
-  
+
   cy.on('drag', 'node', /*_.debounce(*/function( e ){
+
     var node = e.cyTarget;
-    
-    Elements.update({
-      id: node.id()
+    var nodeDoc = Nodes.findOne({ "data.id" : node.id() });
+
+    Nodes.update({
+      _id: nodeDoc._id
     }, {
       $set: { position: node.position() }
     });
   }/*, 100)*/);
 }
 
-// add a new node to the graph
-function addNode(){
-  Elements.insert({
-    group: 'nodes',
-    data: {
-      id: 'node' + Math.round( Math.random() * 1000000 )
-    },
-    position: {
-      x: 0,
-      y: 0
-    }
-  });
-  
-  Meteor.publish();
-}
-
 if( Meteor.isClient ){
-  Meteor.subscribe('elements');
+
+  Meteor.subscribe('nodes');
+  Meteor.subscribe('edges');
   
   $(function(){
     FastClick.attach( document.body );
     
     initCy();
-    
+
+    // add random nodes 
     $('#add').on('click', function(){
-      addNode();
+      Meteor.call("initCyData");
     });
   });
 }
 
-if (Meteor.isClient) {
-  // code is only run on client
-}
-
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    // Meteor.call("destroyCyData"); // reset all nodes on startup
   });
 }
+
+
+Meteor.methods({
+    addNode: function () {
+        Nodes.insert({
+          group: 'nodes',
+          data: {
+            id: 'node' + Math.round( Math.random() * 1000000 )
+          },
+          position: {
+            x: Math.random() *800,
+            y: Math.random() *600
+          }
+        });
+    },
+    addEdge : function (source, target) {
+         Edges.insert({
+            group: 'edges',
+            data: {
+              "source" :source.data.id,
+              "target" :target.data.id
+            }
+          });
+    },
+    deleteNode: function (nodeId) {
+      
+        var node = Nodes.findOne(nodeId);
+        Nodes.remove(nodeId);
+    },
+    initCyData : function(){
+
+        for(i = 0; i < 20; i++)
+            Meteor.call("addNode");
+
+        // add Edges
+        for(i = 0; i < 10; i++){
+          var source = Random.choice(Nodes.find().fetch());
+          var target = Random.choice(Nodes.find({_id:{$ne:source._id}}).fetch());//make sure we dont connect to the source
+
+          Meteor.call("addEdge", source, target);
+
+        }
+    },
+    destroyCyData: function() {
+        Nodes.remove({})
+        Edges.remove({})
+        // Nodes.find({}).forEach(function(node){
+        //     Nodes.remove({_id:node._id});
+        // });
+        // Edges.find({}).forEach(function(edge){
+        //     Edges.remove({_id:edge._id});
+        // });
+    }
+});
